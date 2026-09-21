5317bf75b857e04ff7b5d6aa85119ad225d4585c
DaZayac <minininja2008@yandex.ru>
2026-09-21 13:04:14 +0300

    Refactor sorting by inlining swap logic
    Removed the swap function and replaced its usage with inline swapping.

--- Дз_21.09/simple_implementation/main.cpp
+++ Дз_21.09/simple_implementation/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 
-void swap(int *a, int *b);
 void my_sort(int *arr, const int size);
 
 int main() {
@@ -21,16 +20,14 @@ int main() {
     delete[] arr;
     return 0;
 }
-void swap(int *a, int *b){
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
+
 void my_sort(int *arr, const int size) {
     for (int i = 0;i < size - 1; i++) {
         for (int j = 0; j < size - 1 - i; j++) {
             if (arr[j] > arr[j + 1]) {
-                swap(&arr[j], &arr[j + 1]);
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
             }
         }
     }
